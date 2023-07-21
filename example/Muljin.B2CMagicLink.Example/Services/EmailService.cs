9a782dc288ad4f64b8cfaa9eb39029521f0fdddb
Mahmoud Swehli <mahmoud@muljin.com>
2023-07-21 04:40:07 +0100

    added more advanced login flow for example

--- example/Muljin.B2CMagicLink.Example/Services/EmailService.cs
+++ example/Muljin.B2CMagicLink.Example/Services/EmailService.cs
@@ -9,14 +9,16 @@ namespace Muljin.B2CMagicLink.Example.Services
 {
 	public class EmailService
 	{
-		private static string magicLinkFlowUrl = "https://Muljin.b2clogin.com/Muljin.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1A_SIGNIN_WITH_EMAIL&client_id=64257a7a-3240-4021-8860-45af0bbd6734&nonce=defaultNonce&redirect_uri={0}&scope=openid&response_type=code";
+		//private static string magicLinkFlowUrl = "https://Muljin.b2clogin.com/Muljin.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1A_SIGNIN_WITH_EMAIL&client_id=64257a7a-3240-4021-8860-45af0bbd6734&nonce=defaultNonce&redirect_uri={0}&scope=openid&response_type=code";
+
+		private readonly string magicLinkFlowUrl;
 		private readonly SendGridOptions _sendGridOptions;
 
         public EmailService(IOptions<SendGridOptions> sendGridOptions,
 			IOptions<AzureAdB2cOptions> azureAdB2cOptions)
 		{
 			_sendGridOptions = sendGridOptions.Value ?? throw new ArgumentNullException(nameof(sendGridOptions));
-			magicLinkFlowUrl = magicLinkFlowUrl.Replace("{0}", HttpUtility.HtmlEncode(azureAdB2cOptions.Value.RedirectUrl));
+			magicLinkFlowUrl = $"{azureAdB2cOptions.Value.RedirectUrl}";
 		}
 
 		public async Task SendMagicLinkAsync(string email, string token)
@@ -28,9 +30,9 @@ namespace Muljin.B2CMagicLink.Example.Services
 			var to = new EmailAddress(email);
 
 
-			var content = $"To login, goto {magicLinkFlowUrl}&id_token_hint={token}";
+			var content = $"To login, goto {magicLinkFlowUrl}?id_token_hint={token}";
 
-			var htmlContent = $"To login, <a href=\"{magicLinkFlowUrl}&id_token_hint={token}\"> click here </a>";
+			var htmlContent = $"To login, <a href=\"{magicLinkFlowUrl}?id_token_hint={token}\"> click here </a>";
 
             var msg = MailHelper.CreateSingleEmail(from, to, "Your Muljin Magic Link Example login link", content, htmlContent);
 
