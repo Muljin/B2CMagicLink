52c313156bb1fb7c2901316be6844c696227fece
Mahmoud Swehli <mahmoud@muljin.com>
2023-09-13 15:57:01 +0800

    added additional methods to create jwt tokens

--- src/Muljin.B2CMagicLink/Muljin.B2CMagicLink.Abstractions/IOidcService.cs
+++ src/Muljin.B2CMagicLink/Muljin.B2CMagicLink.Abstractions/IOidcService.cs
@@ -12,9 +12,37 @@ public interface IOidcService
     /// <returns></returns>
     public string GetSerializedOidcMetadata();
 
+    /// <summary>
+    /// Get the JWKs
+    /// </summary>
+    /// <returns></returns>
     public string GetSerializedJwks();
 
+    /// <summary>
+    /// Build a serialised JWT token with a list of claims
+    /// </summary>
+    /// <param name="audience"></param>
+    /// <param name="duration"></param>
+    /// <param name="claims"></param>
+    /// <returns></returns>
     public Task<string> BuildSerializedIdTokenAsync(string audience, int duration, List<System.Security.Claims.Claim> claims);
 
-    public Task<string> BuildSerializedIdTokenAsync(string audience, int duration, string email); 
+    /// <summary>
+    /// Build a serialized JWT token with the user email as a claim
+    /// </summary>
+    /// <param name="audience"></param>
+    /// <param name="duration"></param>
+    /// <param name="email"></param>
+    /// <returns></returns>
+    public Task<string> BuildSerializedIdTokenByEmailAsync(string audience, int duration, string email);
+
+    /// <summary>
+    /// Build a serialized JWT token with the user object ID as a claim
+    /// </summary>
+    /// <param name="audience"></param>
+    /// <param name="duration"></param>
+    /// <param name="objectId"></param>
+    /// <returns></returns>
+    public Task<string> BuildSerializedIdTokenByObjectIdAsync(string audience, int duration, string objectId);
+
 }
