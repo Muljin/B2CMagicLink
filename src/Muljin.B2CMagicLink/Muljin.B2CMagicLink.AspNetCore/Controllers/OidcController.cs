db14c85c89ee67251bd31a348f39555e4b56b53f
Mahmoud Swehli <mahmoud@muljin.com>
2023-09-26 15:05:16 +0800

    updated packages

---
+++ src/Muljin.B2CMagicLink/Muljin.B2CMagicLink.AspNetCore/Controllers/OidcController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace Muljin.B2CMagicLink.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OidcController : ControllerBase
+    {
+        private readonly IOidcService _odicService;
+
+        public OidcController(IOidcService odicService)
+        {
+            _odicService = odicService;
+        }
+
+        [Route(".well-known/openid-configuration", Name = "OIDCMetadata")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public ActionResult Metadata()
+        {
+            var meta = _odicService.GetSerializedOidcMetadata();
+            return Content(meta, "application/json");
+        }
+
+        [Route(".well-known/keys", Name = "JWKS")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public ActionResult JwksDocument()
+        {
+            var jwks = _odicService.GetSerializedJwks();
+            return Content(jwks, "application/json");
+        }
+    }
+}
+
